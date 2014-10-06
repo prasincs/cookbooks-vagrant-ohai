de2ba5542c7c8471179c82e598a006e7cc0249f1
Prasanna Gautam <prasannagautam@gmail.com>
2014-10-05 23:51:05 -0400

    Update vagrant-net.rb

--- files/default/plugins/vagrant-net.rb
+++ files/default/plugins/vagrant-net.rb
@@ -1,10 +1,11 @@
 
 # borrowed from https://gist.github.com/2050259
-
-provide "ipaddress"
-require_plugin "#{os}::network"
-network["interfaces"]["eth1"]["addresses"].each do |ip, params|
-  if params['family'] == ('inet')
-    ipaddress ip
+Ohai.plugin(:Vagrant) do
+  provide "ipaddress"
+  require_plugin "#{os}::network"
+  network["interfaces"]["eth1"]["addresses"].each do |ip, params|
+    if params['family'] == ('inet')
+      ipaddress ip
+    end
   end
 end
